a9cc6ffd03b0f35a2c2235deea70b3a5fa5054ce
fn1z <mr.artemrud2005@gmai.com>
2026-04-17 12:58:35 +0300

    Add book intro structure

---
+++ src/main/java/com/example/onlinebookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.example.onlinebookstore.repository;
+
+import com.example.onlinebookstore.model.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class BookRepositoryImpl implements BookRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Book save(Book book) {
+        entityManager.persist(book);
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return entityManager
+                .createQuery("from Book", Book.class)
+                .getResultList();
+    }
+}
---
+++ src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.onlinebookstore.service;
+
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    @Transactional
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
