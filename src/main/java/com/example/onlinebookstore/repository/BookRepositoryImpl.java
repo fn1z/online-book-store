b60fa82a8b3efc079f9b4220954e00a3c82edb01
fn1z <mr.artemrud2005@gmai.com>
2026-04-20 10:21:43 +0300

    fix mentor review comments

--- src/main/java/com/example/onlinebookstore/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/onlinebookstore/repository/BookRepositoryImpl.java
@@ -1,28 +1,43 @@
 package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.Book;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManagerFactory;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public class BookRepositoryImpl implements BookRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+    }
 
     @Override
     public Book save(Book book) {
-        entityManager.persist(book);
-        return book;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book: " + book, e);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return entityManager
-                .createQuery("from Book", Book.class)
-                .getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Book", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books", e);
+        }
     }
 }
--- src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
@@ -3,25 +3,20 @@ package com.example.onlinebookstore.service;
 import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     @Override
-    @Transactional
     public Book save(Book book) {
         return bookRepository.save(book);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
